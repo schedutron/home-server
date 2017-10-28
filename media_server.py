dc0b34d53ce7f9fe4e48788ae8f5a790e06ca2ec
Thomas Dargent <thomasdargent@gmail.com>
2017-10-28 18:36:18 +0200

    Fixed the problem with css It seems that for your programs to read the css files, it need to be in the static folder. But to avoid listing it with the video files i changed the video directory to /static/video. Now it seems to work. Also changed a mistakes I made : referenced the p by a class .link, but never out the class in the concerned files... So remplaced.link by p. now it works.

--- media_server.py
+++ media_server.py
@@ -3,7 +3,7 @@ from flask import Flask, request, render_template
 
 reload(sys)
 sys.setdefaultencoding('utf8')
-video_dir = 'static'
+video_dir = 'static/video/'
 
 app = Flask(__name__)
 
@@ -13,7 +13,6 @@ def index():
     video_files = [f for f in os.listdir(video_dir)]
     video_files_number = len(video_files)
     return render_template("index.html", title='Home', video_files_number=video_files_number, video_files=video_files)
-
 @app.route('/<filename>')
 def video(filename):
     return render_template('play.html', title=filename, video_file=filename)
