9c5a5a72be66f1c556b217ccf7fe2f57cc5fac08
cnpapado <14839869+cnpapado@users.noreply.github.com>
2019-09-30 17:57:38 +0300

    added db

---
+++ models.py
@@ -0,0 +1,6 @@
+from media_server import db
+
+class Video(db.Model):
+    id = db.Column(db.Integer, primary_key=True)
+    filename = db.Column(db.String(64), index=True, unique=True)
+    likes = db.Column(db.Integer, index=True, default=0)
