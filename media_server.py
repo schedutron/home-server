ecf75e1bdb280e07d75ba5f461ba278617828d85
capt titanium <tanishagarwal81201@gmail.com>
2020-10-07 15:56:43 +0530

    improved some animations

--- media_server.py
+++ media_server.py
@@ -1,4 +1,5 @@
-import os, sys
+import os
+import sys
 from flask import Flask, request, render_template
 
 reload(sys)
@@ -7,17 +8,24 @@ video_dir = 'static/video/'
 
 app = Flask(__name__)
 
+
 @app.route('/')
 @app.route('/home')
 def index():
     video_files = [f for f in os.listdir(video_dir)]
     video_files_number = len(video_files)
     return render_template("index.html", title='Home', video_files_number=video_files_number, video_files=video_files)
+
+
 @app.route('/<filename>')
 def video(filename):
     return render_template('play.html', title=filename, video_file=filename)
+
+
 @app.route('/test')
 def test():
     return render_template('test.html')
+
+
 if __name__ == '__main__':
     app.run(host='0.0.0.0', debug=True)
